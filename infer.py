e93df743066235a115c9b1c41fa8556a6da8d934
Oraoto <melegant2@gmail.com>
2018-08-15 17:53:33 +0800

    init

---
+++ infer.py
@@ -0,0 +1,48 @@
+from __future__ import absolute_import
+
+import paddle.fluid as fluid
+import paddle.fluid.io as io
+import numpy as np
+import sys
+from PIL import Image
+from keras.applications.vgg16 import decode_predictions
+
+from vgg16 import VGG16
+
+## Read image and preprocess
+im = Image.open(sys.argv[1])
+
+im = im.resize((224, 224), Image.ANTIALIAS)
+im = np.array(im).astype(np.float32)
+im = im.transpose((2, 0, 1))  # CHW
+im = im[(2, 1, 0), :, :]  # BGR
+
+mean = np.array([104., 117., 124.], dtype=np.float32)
+mean = mean.reshape([3, 1, 1])
+im = im - mean
+im = np.expand_dims(im, 0)
+
+## Define the network
+input = fluid.layers.data('img', shape=[3, 224, 224])
+predict = VGG16(include_top=True, infer=True).net(input)
+
+## Create executor
+place = fluid.CUDAPlace(0) if fluid.core.is_compiled_with_cuda() else fluid.CPUPlace()
+exe = fluid.Executor(place)
+
+## Load params
+io.load_params(exe, "models")
+
+## Create inference program
+infer_program = fluid.default_main_program().clone(for_test=True)
+infer_program.prune(predict)
+
+## Run it
+p = exe.run(infer_program, fetch_list=[predict], feed={
+    'img': im
+})
+
+p = decode_predictions(p[0])
+
+for (_, cls, prob) in p[0]:
+    print("{}: {}".format(cls, prob))
---
+++ vgg16.py
@@ -0,0 +1,73 @@
+import paddle.fluid as fluid
+
+class VGG16():
+
+    def __init__(self, include_top=False, infer=True):
+        self.include_top = include_top
+        self.infer = infer
+
+    def _conv2d(self, input, num_filters, name):
+        return fluid.layers.conv2d(
+            input,
+            num_filters=num_filters,
+            filter_size=3,
+            padding=1,
+            param_attr=name + "_weights",
+            bias_attr=name + "_biases",
+            act='relu')
+
+    def _maxpool(self, input):
+        return fluid.layers.pool2d(
+            input,
+            pool_size=2,
+            pool_stride=2,
+            ceil_mode=True)
+
+    def _fc(self, input, size, act, name=""):
+        return fluid.layers.fc(
+            input,
+            size=size,
+            act=act,
+            param_attr=name + "_weights",
+            bias_attr=name + "_biases")
+
+    def net(self, input):
+        y = input
+
+        y = self._conv2d(y, num_filters=64, name="vgg16/conv1_1")
+        y = self._conv2d(y, num_filters=64, name="vgg16/conv1_2")
+        y = self._maxpool(y)
+
+        y = self._conv2d(y, num_filters=128, name="vgg16/conv2_1")
+        y = self._conv2d(y, num_filters=128, name="vgg16/conv2_2")
+        y = self._maxpool(y)
+
+        y = self._conv2d(y, num_filters=256, name="vgg16/conv3_1")
+        y = self._conv2d(y, num_filters=256, name="vgg16/conv3_2")
+        y = self._conv2d(y, num_filters=256, name="vgg16/conv3_3")
+        y = self._maxpool(y)
+
+        y = self._conv2d(y, num_filters=512, name="vgg16/conv4_1")
+        y = self._conv2d(y, num_filters=512, name="vgg16/conv4_2")
+        y = self._conv2d(y, num_filters=512, name="vgg16/conv4_3")
+        y = self._maxpool(y)
+
+        y = self._conv2d(y, num_filters=512, name="vgg16/conv5_1")
+        y = self._conv2d(y, num_filters=512, name="vgg16/conv5_2")
+        y = self._conv2d(y, num_filters=512, name="vgg16/conv5_3")
+        y = self._maxpool(y)
+
+        if not self.include_top:
+            return y
+
+        y = self._fc(y, size=4096, name="vgg16/fc6", act='relu')
+        if not self.infer:
+            y = fluid.layers.dropout(y, dropout_prob=0.5)
+
+        y = self._fc(y, size=4096, name="vgg16/fc7", act='relu')
+        if not self.infer:
+            y = fluid.layers.dropout(y, dropout_prob=0.5)
+
+        y = self._fc(y, size=4096, name="vgg16/fc8", act='softmax')
+
+        return y
